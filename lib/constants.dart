1b12e201b5ed9cae0220f3259966531fd18f0e54
Leon Gustafsson <gustafsson.leon1@gmail.com>
2021-04-24 19:41:39 +0300

    v3

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const widgetcolor = Color(0xff54692e);
+const textcolor = Color(0xffbfc0c2);
+const backgroundcolor = Color(0xff364815);
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:shapevisor/pages/home_page.dart';
-import 'package:shapevisor/pages/login_page.dart';
+import 'package:shapevisor/constants.dart';
+import 'package:shapevisor/screens/homepage.dart';
 
-void main() => runApp(MaterialApp(
-  home: Login(),
-));
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ShapeVisor',
+      theme: ThemeData(
+        scaffoldBackgroundColor: backgroundcolor,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: textcolor),
+        primaryColor: widgetcolor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
 
