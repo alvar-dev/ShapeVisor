94346c79f6915ba323d4aaeeae91abfa8ecbb90d
Leon Gustafsson <gustafsson.leon1@gmail.com>
2021-05-04 20:59:15 +0300

    uudet v2

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(80.0),
+            child: AppBar(
+              elevation: 0,
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/dumbel.png'),
+          ),
+        ),
+      ),
     );
   }
 }
