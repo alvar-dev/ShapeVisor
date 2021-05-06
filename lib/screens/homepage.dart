6fe46ee89036c146a294df1ad7af65310c77b65e
Sisu Eriksson <sisu.eriksson@gmail.com>
2021-05-06 20:07:45 +0300

    Rakenne 1

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,10 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Container(
+        child: Row(
+          children: [
+            Column(
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/dumbel.png'
+                  ),
+                  radius: 35,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
-  }
+}
 }
