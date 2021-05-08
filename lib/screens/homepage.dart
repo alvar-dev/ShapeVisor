41a4f93b5d45717c71d46a215b838f7dbe13a83b
Leon Gustafsson <gustafsson.leon1@gmail.com>
2021-05-08 15:55:45 +0300

    uudet v3

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -5,16 +5,23 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          ClipOval(
+            child: Image.asset('assets/dumbel.png', width: 70, height: 70, fit: BoxFit.cover),
+          ),
+        ],
+      ),
+      ),
       body: Container(
         child: Row(
           children: [
             Column(
               children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/dumbel.png'
-                  ),
-                  radius: 35,
-                ),
+
               ],
             ),
           ],
