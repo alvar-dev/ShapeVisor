e962f0b48d3577ddbb28ab9f8a30e2c8753c86ef
Leon Gustafsson <gustafsson.leon1@gmail.com>
2021-05-11 14:13:57 +0300

    uudet v6

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -12,35 +12,46 @@ class HomeScreen extends StatelessWidget {
           children: <Widget>[
             Column(
               children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(40.0),
-                    ),
-                    color: Colors.red,
-                  ),
-                  padding: EdgeInsets.fromLTRB(165, 35, 165, 35),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundImage: AssetImage('assets/dumbel.png'),
-                        radius: 40,
+                Stack(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(40.0),
+                        ),
+                        color: Colors.blue,
                       ),
-                    ],
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(40.0),
+                      //padding: EdgeInsets.fromLTRB(205.5, 70, 205.5, 70),
+                      height: 200,
+                      width: 411.4,
                     ),
-                    color: Colors.blue,
-                  ),
-                  padding: EdgeInsets.fromLTRB(205.5, 70, 205.5, 70),
+                    Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(40.0),
+                        ),
+                        color: Colors.red,
+                      ),
+                      //padding: EdgeInsets.fromLTRB(165, 0, 165, 0),
+                      height: 120,
+                      width: 411.4,
+
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          SizedBox(width: 50),
+                          CircleAvatar(
+                            backgroundImage: AssetImage('assets/dumbel.png'),
+                            radius: 40,
+                          ),
+                          SizedBox(width: 10),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
